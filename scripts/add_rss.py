ab914a571a38f0b44cb5ad1b29e4e28961c5d7bb
asdf <asdf>
2022-10-24 08:03:55 -0300

    started .gitignore and wrote add_rss.py

--- scripts/add_rss.py
+++ scripts/add_rss.py
@@ -5,4 +5,21 @@ from replit import db
 
 
 def parse_args():
-  
+    parser = argparse.ArgumentParser()
+    parser.add_argument("url", help="URL of RSS feed")
+    parser.add_argument("name", help="Name of RSS feed")
+
+    return parser.parse_args(sys.argv[1:])
+
+
+def add_rss(url: str, name: str):
+    db["rss_feeds"][name] = url
+
+
+def main():
+    args = parse_args()
+    add_rss(args.url, args.name)
+
+
+if __name__ == "__main__":
+    main()
